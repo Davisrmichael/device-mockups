7d5ed83514d1cfa2b9cfc99e5269cdb9c168c6a4
Davisrmichael <michaeld.0312@gmail.com>
2025-08-10 13:00:44 -0600

    Add files via upload

--- js/export-viewport.js
+++ js/export-viewport.js
@@ -1,12 +1,1 @@
-export function makeExporter(viewer) {
-  const { renderer } = viewer;
-
-  async function exportViewerPNG() {
-    // Ensure current frame rendered
-    renderer.render(viewer.scene, viewer.camera);
-    // toDataURL will capture exactly the 720×720 canvas (because we sized it that way)
-    return renderer.domElement.toDataURL('image/png');
-  }
-
-  return { exportViewerPNG };
-}
+// placeholder export-viewport.js
